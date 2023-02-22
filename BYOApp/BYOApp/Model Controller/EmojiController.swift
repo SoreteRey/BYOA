30ca86de1d7c6b8e840cacda642dd6638f779ab2
sguiscardo <sdguiscardo@gmail.com>
2023-02-22 17:46:07 -0600

    running app

---
+++ BYOApp/BYOApp/Model Controller/EmojiController.swift
@@ -0,0 +1,8 @@
+//
+//  EmojiController.swift
+//  BYOApp
+//
+//  Created by Sebastian Guiscardo on 2/22/23.
+//
+
+import Foundation
---
+++ BYOApp/BYOApp/Model/Emoji.swift
@@ -0,0 +1,8 @@
+//
+//  Emoji.swift
+//  BYOApp
+//
+//  Created by Sebastian Guiscardo on 2/22/23.
+//
+
+import Foundation
